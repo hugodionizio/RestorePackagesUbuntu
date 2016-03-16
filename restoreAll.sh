0e3ad9512f639eb8923464f26dd253e4c8c7599d
Hugo Dionizio Santos <hugodionizio@users.noreply.github.com>
2016-03-16 17:50:26 -0300

    Priorizando instalação por necessidades

--- restoreAll.sh
+++ restoreAll.sh
@@ -1,25 +1,3 @@
-# Google Chrome
-sudo apt-get install chromwget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable
-
-# Grive
-sudo add-apt-repository ppa:nilarimogard/webupd8
-sudo apt-get update
-sudo apt-get install grive
-
-# Licq
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
-sudo apt-get update
-sudo apt-get install licq
-
-# Scibuntu
-wget http://urban.it.hik.se/scibuntu/scibuntu021-alfa
-sudo ./scibuntu041-beta 
-sudo apt-get install git
-
 # Oracle Java
 sudo add-apt-repository ppa:webupd8team/java && sudo apt-get --quiet update
 sudo apt-get install oracle-java7-installer
@@ -31,3 +9,25 @@ sudo add-apt-repository "deb http://ppa.launchpad.net/eclipse-team/ppa/ubuntu $(
 sudo add-apt-repository "deb-src http://ppa.launchpad.net/eclipse-team/ppa/ubuntu $(lsb_release -sc) main"
 sudo apt-get update
 sudo apt-get install eclipse-cdt
+
+# Google Chrome
+sudo apt-get install chromwget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+# Licq
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+sudo apt-get update
+sudo apt-get install licq
+
+# Grive
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install grive
+
+# Scibuntu
+wget http://urban.it.hik.se/scibuntu/scibuntu021-alfa
+sudo ./scibuntu041-beta 
+sudo apt-get install git
